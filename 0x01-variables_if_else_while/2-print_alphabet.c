55d8e2cdab6e2b1c9a7ce1ec999005d28bde9003
Florent Fayet <florent.fayet20@gmail.com>
2021-10-04 15:50:54 +0200

    fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-    char c = 'a';
+	  char c = 'a';
 
-    while (c <= 'z')
+	while (c <= 'z')
 
-    {
-    	putchar(c);
-    	c++;
-    }
+	{
+		putchar(c);
+		c++;
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
